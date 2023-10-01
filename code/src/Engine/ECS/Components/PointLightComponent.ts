42959e002ad100e6796caee8223db15008fa8a35
Praccen <gbjorken96@hotmail.com>
2023-10-01 02:15:05 +0200

    Point light component gui connections.

--- code/src/Engine/ECS/Components/PointLightComponent.ts
+++ code/src/Engine/ECS/Components/PointLightComponent.ts
@@ -1,5 +1,8 @@
+import ObjectPlacer from "../../../Game/ObjectPlacer";
+import Div from "../../GUI/Div";
 import PointLight from "../../Lighting/PointLight";
 import Vec3 from "../../Maths/Vec3";
+import { OverlayRendering } from "../../Rendering/OverlayRendering";
 import { Component, ComponentTypeEnum } from "./Component";
 
 export default class PointLightComponent extends Component {
@@ -10,5 +13,27 @@ export default class PointLightComponent extends Component {
 		super(ComponentTypeEnum.POINTLIGHT);
 		this.pointLight = pointLight;
 		this.posOffset = new Vec3();
+	}
+
+	addToGui(
+		overlayRendering: OverlayRendering,
+		parentDiv: Div,
+		objectPlacer: ObjectPlacer
+	) {
+		let addTextEdit = (label: string, vec: Vec3, index: number) => {
+			let propEditText = overlayRendering.getNewEditText(parentDiv);
+			propEditText.textString = label;
+			propEditText.textSize = 20;
+			propEditText.scaleWithWindow = true;
+			propEditText.getInputElement().value = vec[index].toString();
+			propEditText.getInputElement().onchange = (ev) => {
+				vec[index] = parseFloat(propEditText.getInputElement().value);
+			};
+			objectPlacer.makeCheckpoint();
+		};
+
+		addTextEdit("R", this.pointLight.colour, 0);
+		addTextEdit("G", this.pointLight.colour, 1);
+		addTextEdit("B", this.pointLight.colour, 2);
 	}
 }
